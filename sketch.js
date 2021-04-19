c344fc9b6e5448e2013f3465dfd58fad765c304c
Darsh845 <79397989+Darsh845@users.noreply.github.com>
2021-04-19 19:43:44 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,6 @@ car = createSprite(50, 200, 50, 50);
  
  car.velocityX = speed ;
  
- car.shapeColor=color(255);
  
 wall = createSprite(1500, 200, 60, height/2);
  wall.shapeColor=color(80,80,80)
